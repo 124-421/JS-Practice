5cfad8f7c8754b9da206d391c705424b251c2364
Hemant Singh Bisht <hs92595@gmail.com>
2025-12-04 13:04:34 +0000

    added 2 files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 124421
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState ;
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.log(accountState)
